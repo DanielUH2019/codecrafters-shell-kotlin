5b98ba0ee6f907f7a7302855f0109b2e020f8084
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2024-12-07 19:34:16 -0500

    fourth stage

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,20 @@
+import kotlin.system.exitProcess
+
 fun main() {
     do {
         print("$ ")
-        val command = readln()
-        println("$command: command not found")
+        val userInput = readln()
+        val splittedUserInput = userInput.split(" ")
+        val command = splittedUserInput[0]
+        val args = if (splittedUserInput.size > 1) splittedUserInput.subList(1, splittedUserInput.size) else null
+
+        when (command) {
+            "exit" -> {
+                val exitCode = args?.getOrNull(0)?.toIntOrNull() ?: 0 // Parse exit code or default to 0
+                exitProcess(exitCode)
+            }
+            else -> println("$command: command not found")
+        }
+
     } while (true)
 }
