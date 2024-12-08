7cf7a6cc6a5b0c4f2a95b756e3deafba79a2ea65
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2024-12-07 20:03:07 -0500

    sixth stage

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,14 @@
 import kotlin.system.exitProcess
 
+
+
 fun main() {
     do {
         print("$ ")
         val userInput = readln()
-        val splittedUserInput = userInput.split(" ")
-        val command = splittedUserInput[0]
-        val args = if (splittedUserInput.size > 1) splittedUserInput.subList(1, splittedUserInput.size) else null
+        val splitUserInput = userInput.split(" ")
+        val command = splitUserInput[0]
+        val args = if (splitUserInput.size > 1) splitUserInput.subList(1, splitUserInput.size) else null
 
         when (command) {
             "exit" -> {
@@ -14,8 +16,23 @@ fun main() {
                 exitProcess(exitCode)
             }
             "echo" -> println(args?.joinToString(" ") ?: "")
+            "type" -> runTypeBuiltin(args?.getOrNull(0) ?: "")
             else -> println("$command: command not found")
         }
 
     } while (true)
+}
+
+fun runTypeBuiltin(command: String) {
+    if (command.isEmpty()) {
+        println()
+        return
+    }
+    val isSupported = when (command) {
+        "exit" -> true
+        "echo" -> true
+        "type" -> true
+        else -> false
+    }
+    if (isSupported) println("$command is a shell builtin") else println("$command: not found")
 }
