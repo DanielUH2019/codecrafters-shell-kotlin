3400f702f1897386df17a02c7278e22421c9b305
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2024-12-16 13:09:48 -0500

    allow to run executables

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import java.io.IOException
 import kotlin.io.path.*
 import kotlin.system.exitProcess
 import java.nio.file.Path
@@ -19,9 +20,8 @@ fun main() {
             }
             "echo" -> println(args?.joinToString(" ") ?: "")
             "type" -> runTypeBuiltin(args?.getOrNull(0) ?: "", pathDirectories)
-            else -> println("$command: command not found")
+            else -> runCommandIfIsInPath(command, args ?: listOf<String>(), pathDirectories)
         }
-
     } while (true)
 }
 
@@ -52,4 +52,26 @@ fun runTypeBuiltin(command: String, pathDirectories: List<String>) {
         }
     }
     println("$command: not found")
+}
+
+fun runCommandIfIsInPath(command: String, args: List<String>, pathDirectories: List<String>) {
+    pathDirectories.forEach { path ->
+        val commandFile = Path.of(path, command)
+        if (commandFile.exists() && commandFile.isExecutable()) {
+            try {
+                val process = ProcessBuilder(listOf(commandFile.toString()) + args)
+                    .inheritIO() // Ensures the process uses the same I/O as the parent process
+                    .start()
+                process.waitFor() // Wait for the process to complete
+                return
+            } catch (e: IOException) {
+                println("Error executing command: ${e.message}")
+                return
+            }
+        }
+        else {
+            println("$command: command not found")
+            return
+        }
+    }
 }
